b5e10185edd046e0aec385db8f0f7a8c7c0c6fbb
Ni1k1ta <145503928+Ni1k1ta@users.noreply.github.com>
2023-10-03 17:20:33 -0300

    Comit Carousel

--- JS/+info.js
+++ JS/+info.js
@@ -1,44 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
-    var botonCabañaInfo = document.getElementById("cabañainfo");
-    var botonBungalowsInfo = document.getElementById("bungalowsinfo");
-    var botonCabañaReserva = document.getElementById("cabañareserva");
-    var botonBungalowsReserva = document.getElementById("bungalowsreserva");
-  
-    
-    botonCabañaInfo.addEventListener("click", function () {
-      toggleInfo(document.querySelector(".cabañasbox"), botonCabañaInfo);
-    });
-  
-    
-    botonBungalowsInfo.addEventListener("click", function () {
-      toggleInfo(document.querySelector(".bungalowsbox"), botonBungalowsInfo);
-    });
-  
-    
-    botonCabañaReserva.addEventListener("click", function () {
-      mostrarReserva();
-    });
-  
-    
-    botonBungalowsReserva.addEventListener("click", function () {
-      mostrarReserva();
-    });
-  
-    
-    function toggleInfo(box, boton) {
-      box.classList.toggle("info-visible"); 
-      if (box.classList.contains("info-visible")) {
-        boton.textContent = "-Info"; 
-      } else {
-        boton.textContent = "+Info"; 
-      }
-    }
-  
-    
-    function mostrarReserva() {
-      var sectionReserva = document.querySelector(".section-reserva");
-      sectionReserva.classList.add("mostrar"); 
-    }
+  var botonCabañaInfo = document.getElementById("cabañainfo");
+  var botonBungalowsInfo = document.getElementById("bungalowsinfo");
+  var botonCabañaReserva = document.getElementById("cabañareserva");
+  var botonBungalowsReserva = document.getElementById("bungalowsreserva");
+
+  botonCabañaInfo.addEventListener("click", function () {
+    toggleInfo(document.querySelector(".cabañasbox"), botonCabañaInfo);
   });
-  
+
+  botonBungalowsInfo.addEventListener("click", function () {
+    toggleInfo(document.querySelector(".bungalowsbox"), botonBungalowsInfo);
+  });
+
+  botonCabañaReserva.addEventListener("click", function () {
+    mostrarReserva();
+  });
+
+  botonBungalowsReserva.addEventListener("click", function () {
+    mostrarReserva();
+  });
+
+  function toggleInfo(box, boton) {
+    box.classList.toggle("info-visible");
+    if (box.classList.contains("info-visible")) {
+      boton.textContent = "-Info";
+    } else {
+      boton.textContent = "+Info";
+    }
+  }
+
+  function mostrarReserva() {
+    var sectionReserva = document.querySelector(".section-reserva");
+    sectionReserva.classList.add("mostrar");
+  }
+});
+/* carrrusel header /*/
+//////////////////////////////
+
+const carousel = document.querySelector(".carousel");
+
+let intervalo=null;
+let step=1;
+const start = () => {
+  intervalo = setInterval(function name() {
+    carousel.scrollLeft = carousel.scrollleft + 10;
+  }, 3);
+};
+const stop = () => {};
+
+start();
